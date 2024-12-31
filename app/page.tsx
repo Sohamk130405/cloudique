f034fdec557ac2c60fad18bc5be29755642e026a
Soham Kolhatkar <sohamk130405@gmail.com>
2024-12-31 19:54:39 +0530

    config setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 const Home = () => {
   return (
     <div>
-      <h1>Cloudique - A only storage solution you need.</h1>
+      <h1 className="text-brand">Cloudique - A only storage solution you need.</h1>
     </div>
   );
 };
