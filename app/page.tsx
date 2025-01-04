29794619db1a82ce35f3a41cce28f1b86ab5fa0f
Soham Kolhatkar <sohamk130405@gmail.com>
2025-01-04 16:36:15 +0530

    configurations

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div>
-      <h1 className="text-brand">Cloudique - A only storage solution you need.</h1>
-    </div>
-  );
-};
-
-export default Home;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100mb",
+    },
+  },
 };
 
 export default nextConfig;
