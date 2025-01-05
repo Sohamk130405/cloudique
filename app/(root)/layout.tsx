4b088baf49ed1a3c89d7ebf879778d0d927e0e2e
Soham Kolhatkar <sohamk130405@gmail.com>
2025-01-05 12:05:59 +0530

    dynamic force

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,6 +6,8 @@ import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
   if (!currentUser) return redirect("/sign-in");
@@ -14,10 +16,11 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
     <main className="flex h-screen">
       <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation {...currentUser} /> <Header userId={currentUser.$id} accountId={currentUser.accountId} />
+        <MobileNavigation {...currentUser} />{" "}
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <div className="main-content">{children}</div>
       </section>
-      <Toaster/>
+      <Toaster />
     </main>
   );
 };
