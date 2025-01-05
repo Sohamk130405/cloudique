ccfa168c2187cd82c71939de2d62b7e9f2320693
Soham Kolhatkar <sohamk130405@gmail.com>
2025-01-05 12:21:50 +0530

    error handler fix

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -4,14 +4,11 @@ import { createAdminClient, createSessionClient } from "@/lib/appwrite";
 import { InputFile } from "node-appwrite/file";
 import { appwriteConfig } from "@/lib/appwrite/config";
 import { ID, Models, Query } from "node-appwrite";
-import { constructFileUrl, getFileType, parseStringify } from "@/lib/utils";
+import { constructFileUrl, getFileType, handleError, parseStringify } from "@/lib/utils";
 import { revalidatePath } from "next/cache";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 
-export const handleError = (error: unknown, message: string) => {
-  console.log(error, message);
-  throw error;
-};
+
 
 export const uploadFile = async ({
   file,
@@ -61,7 +58,7 @@ export const uploadFile = async ({
   }
 };
 
-export const createQueries = (
+const createQueries = (
   currentUser: Models.Document,
   types: string[],
   searchText: string,
